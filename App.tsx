4522919c7b04cd498c84f5d353af96597ae983e3
wbrunovieira <wbrunovieira77@gmail.com>
2023-01-24 11:51:24 -0300

    added onesignal to android

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +9,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId('570a1972-0dc8-49f2-b1af-e65700301043')
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
